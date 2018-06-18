82f2f93fec719978cf08b16abedcc1f9efe85172
Nouran Samy <only1nour@ymail.com>
2018-06-18 12:51:45 +0200

    Adding new enemies with different speeds

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ var Enemy = function () {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    var startPos = [60, 143, 226];
+    this.y = startPos[Math.floor(Math.random() * 3)];
+    this.speed = (Math.random() * 10) + 2;
+    this.x = -101;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +18,7 @@ Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -38,7 +43,7 @@ class Player {
     }
 
     update() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        this.render();
     }
 
     handleInput(keyPressed) {
@@ -61,7 +66,7 @@ class Player {
                 }
             case 'up':
                 if (this.y === 41.5) {
-                    this.reset();
+                    resetGame();
                     break;
                 }
                 else {
@@ -79,26 +84,31 @@ class Player {
         }
         this.update();
     }
-
-    reset() {
-        this.x = 202;
-        this.y = 373.5;
-        this.render();
-    }
 }
 
+// Function to reset game if reaching the water or collision occurs
+function resetGame() {
+    player.x = 202;
+    player.y = 373.5;
+    allEnemies = [];
+    clearInterval(timing);
+    timing = setInterval(newEnemies, 1000);
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate new enemies and adding them to allEnemies array
+function newEnemies() {
+    var enemy = new Enemy();
+    allEnemies.push(enemy);
+}
+
+// Instantiate new player object
 var player = new Player(202, 373.5);
 
 var allEnemies = [];
-setInterval(function () {
-    var enemy = new Enemy();
-    allEnemies.push(enemy);
-    enemy.render();
-}, 500);
+
+// Add new enemy every second
+var timing = setInterval(newEnemies, 1000);
+
 
 
 // This listens for key presses and sends the keys to your
